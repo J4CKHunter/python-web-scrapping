deec95b998d273b8d0ca43f0582e079677c63cbc
J4CKHunter <erdem_nayin@hotmail.com>
2021-03-26 12:55:20 +0300

    fix a lot of things and add 5th phase

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 import random
 from collections import defaultdict
+from nltk.corpus import wordnet
 
 #deneme linkleri :
 #https://stackoverflow.com/questions/1080411/retrieve-links-from-web-page-using-python-and-beautifulsoup
@@ -227,6 +228,60 @@ class textSimilarity:
     def get_result(self):
         return self.result
 
+class synonymSimilarity:
+    def __init__(self,link1 = { },kelimeler = { },result={}):
+        self.link1=link1
+        self.kelimeler=kelimeler
+
+        req = Request(link1, headers={'User-Agent': 'Mozilla/5.0'})
+        html = urlopen(req).read()
+        soup = BeautifulSoup(html, "lxml")
+
+        # kill all script and style elements
+        for script in soup(["script", "style"]):
+            script.extract()  # rip it out
+
+        # get text
+        text1 = soup.get_text()
+        # break into lines and remove leading and trailing space on each
+        lines1 = (line.strip() for line in text1.splitlines())
+        # break multi-headlines into a line each
+        chunks1 = (phrase.strip() for line in lines1 for phrase in line.split("  "))
+        # drop blank lines
+        text1 = '\n'.join(chunk for chunk in chunks1 if chunk)
+
+
+        def clean_string(text):
+            # text = ''.join([word for word in text if word not in string.punctuation])
+            exclist = string.punctuation
+            table_ = str.maketrans(exclist, ' ' * len(exclist))
+            text = ' '.join(text.translate(table_).split())
+            text = text.lower()
+            text = ' '.join([word for word in text.split() if word not in stop_words])
+
+            return text
+
+        sentences = {text1,kelimeler}
+        cleaned = list(map(clean_string, sentences))
+
+        vectorizer = CountVectorizer().fit_transform(cleaned)
+        vectors = vectorizer.toarray()
+
+        csim = cosine_similarity(vectors)
+
+        def cosine_sim_vectors(vec1, vec2):
+            vec1 = vec1.reshape(1, -1)
+            vec2 = vec2.reshape(1, -1)
+
+            return cosine_similarity(vec1, vec2)[0][0]
+
+        try:
+            self.result = cosine_sim_vectors(vectors[0], vectors[1])
+        except:
+            self.result = 1.0000000000000000
+
+    def get_result(self):
+        return self.result
 
 app = Flask(__name__)
 
@@ -374,20 +429,164 @@ def synonymResult():
     print(get_top_n(dictA))
 
     array = []
+    array2 = []
+    count = 0
+    #for i in range(len(dictA)):
+    for key in dictA.keys():
+        if(key == ''):
+            continue
+        else:
+            keySim = keywordSimilarity(key)
+            array.append(keySim.get_keywordFrekanslari())
+            array2.append(key)
+            count+=1
 
-    for i in range(len(dictA)):
-        for key in dictA.keys():
-            if(key == ''):
-                continue
-            else:
-                keySim = keywordSimilarity(key)
-                array.append(keySim.get_keywordFrekanslari())
+    analinkkeywordArray = {}
+    analinkKeyword = keywordSimilarity(link)
+    analinkkeywordArray = analinkKeyword.get_keywordFrekanslari()
 
     return render_template('synonymResult.html',linkset=linkTree,rows=rows,
                            dictAkeys=get_top_n(dictA).keys(),
                            dictAvalues=get_top_n(dictA).values(),
                            dictAitems=get_top_n(dictA).items(),
-                           keywords=array,len=len(dictA.keys()),)
+                           keywords=array,len=len(dictA.keys()),
+                           links=array2,count=count,analink=link,
+                           alka=analinkkeywordArray)
+
+
+@app.route('/sonadim')
+def sonadim():
+    return render_template('sonadim.html')
+
+@app.route('/sonadimresult', methods=["POST"])
+def sonadimresult():
+    link = request.form.get("link")
+
+    linkset = ""
+    linkset = request.form.get("linkset")
+
+    seperatedLinkset = []
+    seperatedLinkset = linkset.split("\r\n")
+    print("seperated linkset yazdiriliyor")
+    print(seperatedLinkset)
+
+    rows, cols = (len(seperatedLinkset), 3)
+    linkTree = [['a' for i in range(cols)] for j in range(rows)]
+    print(linkTree)
+
+    def fonk(link):
+        bos = ''
+        a = 'a'
+        if (link == a or link == bos):
+            return ''
+        else:
+            link = link
+            req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+            html = urlopen(req).read()
+            soup = BeautifulSoup(html, "lxml")
+
+            links = [item['href'] if item.get('href') is not None else item['src'] for item in
+                     soup.select('[href^="http"], [src^="http"]')]
+            print(links)
+
+            if (len(links) != 0):
+                if (len(links) == 1):
+                    return links.pop(0)
+                else:
+                    return links.pop(random.randint(1, len(links)) - 1)
+            else:
+                return ''
+
+    x = len(seperatedLinkset)
+    for i in range(x):
+        linkTree[i][0] = seperatedLinkset[i]
+        print("işte deneme felan")
+        print(seperatedLinkset[i])
+        linkTree[i][1] = fonk(seperatedLinkset[i])
+        linkTree[i][2] = fonk(linkTree[i][1])
+        print(i)
+        print(". döngüdeyiz linktree durumu")
+        print(linkTree)
+
+    print("linktree yazdırılıyor")
+    print(linkTree)
+
+    keywordArray = {}
+    keySim = keywordSimilarity(link)
+    keywordArray=keySim.get_keywordFrekanslari()
+
+    print(keywordArray)
+
+    def yakinAnlamli(keywordArray):
+        string = ''
+        synonyms = []
+        antonyms = []
+        ## active , black , merit , virtue , interesting
+        for word in keywordArray:
+            for syn in wordnet.synsets(word):
+                for l in syn.lemmas():
+                    synonyms.append(l.name())
+                    string = string + l.name() + ' '
+                    if l.antonyms():
+                        antonyms.append(l.antonyms()[0].name())
+        return string
+
+    esAnlamli = yakinAnlamli(keywordArray)
+    print(esAnlamli)
+
+    skorlar = [['a' for i in range(cols)] for j in range(rows)]
+
+    for i in range(rows):
+        for j in range(cols):
+            if (linkTree[i][j] == ''):
+                skorlar[i][j] = 0.0
+            else:
+                temp = synonymSimilarity(linkTree[i][j], esAnlamli)
+                skorlar[i][j] = temp.get_result()
+                print(skorlar[i][j])
+
+    dictA = defaultdict(list)
+
+    for i in range(rows):
+        for j in range(cols):
+            temp1 = linkTree[i][j]
+            temp2 = skorlar[i][j]
+            dictA[temp1].append(temp2)
+
+    print("dict a yazdiriliyor")
+    print(dictA)
+
+    def get_top_n(dict_elem):
+        result = dict(sorted(dict_elem.items(), key=itemgetter(1), reverse=True))
+        return result
+
+    print(get_top_n(dictA))
+
+    array = []
+    array2 = []
+    count = 0
+    # for i in range(len(dictA)):
+    for key in dictA.keys():
+        if (key == ''):
+            continue
+        else:
+            keySim = keywordSimilarity(key)
+            array.append(keySim.get_keywordFrekanslari())
+            array2.append(key)
+            count += 1
+
+    analinkkeywordArray = {}
+    analinkKeyword = keywordSimilarity(link)
+    analinkkeywordArray = analinkKeyword.get_keywordFrekanslari()
+
+    return render_template('sonadimresult.html', linkset=linkTree, rows=rows,
+                           dictAkeys=get_top_n(dictA).keys(),
+                           dictAvalues=get_top_n(dictA).values(),
+                           dictAitems=get_top_n(dictA).items(),
+                           keywords=array, len=len(dictA.keys()),
+                           links=array2, count=count,analink=link,
+                           esAnlamli=esAnlamli,alka=analinkkeywordArray
+                           )
 
 
 
